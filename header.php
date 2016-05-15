a617fbda2d88b1cb157f20117f88c0fbb4b5a3bc
johnfercher <johnathanfercher22@gmail.com>
2016-05-15 14:34:23 -0300

    styles

---
+++ header.php
@@ -0,0 +1,10 @@
+<div id="header">
+	<div class="header-thing" id="div-image">
+		<img id="main-image" src="images/raspberry-pi-logo.png"/>
+	</div>
+	<div class="header-thing" id="div-title">
+		<center>
+			My Raspberry PI 2 Server :D
+		</center>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="css/global_style.css">
 		<title>
 			Raspberry PI 2
 		</title>
 	</head>
 
 	<body>
-		<?php
-			require_once "utils/markdown/Parsedown.php";
+		<div id="global_content">
+			<?php
+				require_once "utils/markdown/Parsedown.php";
 
-			$markdown = new Parsedown();
-			$text = file_get_contents("md_files/README.md");
-			echo $markdown->text($text);
-		?>
+				include "header.php";
+
+				$markdown = new Parsedown();
+				$text = file_get_contents("md_files/README.md");
+				echo $markdown->text($text);
+			?>
+		</div>
+		
 	</body>
 </html>
