2576d551cdfcbb451d69c35203c956c275db9a95
jfercher <johnathanfercher22@gmail.com>
2016-05-15 16:45:46 -0300

    serial

--- index.php
+++ index.php
@@ -9,13 +9,17 @@
 	<body>
 		<div id="global_content">
 			<?php
-				require_once "utils/markdown/Parsedown.php";
+				//require_once "utils/markdown/Parsedown.php";
+				require_once "utils/serial/php_serial_class.php"
 
 				include "header.php";
 
-				$markdown = new Parsedown();
+				/*$markdown = new Parsedown();
 				$text = file_get_contents("md_files/README.md");
 				echo $markdown->text($text);
+
+				exec("echo '5' > /dev/ttyACM0");*/
+
 			?>
 		</div>
 		
--- utils/arduino/serial_arduino.cpp
+++ utils/arduino/serial_arduino.cpp
@@ -4,5 +4,5 @@
 using namespace std;
 
 int main(){
-	system("echo '5' > /dev/ttyACM0");
+	system("sudo echo '5' > /dev/ttyACM0");
 }
