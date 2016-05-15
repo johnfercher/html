7d4517379dc7308671bd03dc6202cebf811334e8
johnfercher <johnathanfercher22@gmail.com>
2016-05-15 15:23:00 -0300

    arduino

---
+++ utils/arduino/arduino_receive.ino
@@ -0,0 +1,30 @@
+/*
+ * Hello World!
+ *
+ * This is the Hello World! for Arduino. 
+ * It shows how to send data to the computer
+ */
+
+int led = 13;
+boolean on = false;
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  pinMode(led, OUTPUT);
+}
+
+void loop(){                       // run over and over again{
+    if(Serial.available()){
+      int a = Serial.read();
+      Serial.println(a);
+      
+      if(on){
+        on = false;
+        digitalWrite(led, HIGH);
+      }else{
+        digitalWrite(led, LOW);
+        on = true;
+      }
+    }
+    delay(500);
+}
